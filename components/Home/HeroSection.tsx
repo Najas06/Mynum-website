d24d2b68ec7da4141c937ff6cc3b04f887b0135d
Myenum <myenumam@gmail.com>
2025-03-02 21:02:37 +0530

    Update HeroSection.tsx

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -8,11 +8,10 @@ const HeroSection = () => {
       <Spotlight />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Mynum <br /> Builds your Growth.
+          Myenum <br /> Defining values with clarity, consistency, and purpose
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          A subtle yet effective spotlight effect, because the previous version
-          is used a bit too much these days.
+          Defining values with clarity, consistency, and purpose
         </p>
       </div>
     </section>
