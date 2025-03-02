e0500510c2844fc3f7d4cd6131830553ecdf7753
Myenum <myenumam@gmail.com>
2025-03-02 21:07:32 +0530

    Update HeroSection.tsx

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
       <Spotlight />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Myenum <br /> Defining values with clarity, consistency, and purpose
+          Myenum <br /> Smart, Seamless, and Future-Ready
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
           Defining values with clarity, consistency, and purpose
