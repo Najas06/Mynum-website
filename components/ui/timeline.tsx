90a143b85d5fc84023740d3bf6d8d1ee97e8355d
Myenum <myenumam@gmail.com>
2025-03-03 21:13:03 +0530

    Update timeline.tsx

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -42,8 +42,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           Changelog from my journey
         </h2>
         <p className="text-neutral-300 text-sm md:text-base max-w-sm">
-          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-          a timeline of my journey.
+          I've been working for the past two years—here's a timeline of my journey.
         </p>
       </div>
 
